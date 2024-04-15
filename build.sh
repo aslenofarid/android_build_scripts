ba3f76f4e463a10a8923dda430b1f6245b7b7568
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-04-15 16:24:13 +0530

    build.sh: Add crave clone create support

--- build.sh
+++ build.sh
@@ -4,17 +4,36 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # Initialize repo with specified manifest
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+export PROJECTFOLDER="/crave-devspaces/Lineage20"
+export PROJECTID="36"
+export REPO_INIT="repo init -u https://github.com/accupara/los20.git -b lineage-20.0 --git-lfs --depth=1"
+if grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"') && [ "${DCDEVSPACE}" == "1" ]; then
+   crave clone destroy -y $PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
+else
+   rm -rf $PROJECTFOLDER || true
+fi
+
+if [ "${DCDEVSPACE}" == "1" ]; then
+   crave clone create --projectID $PROJECTID $PROJECTFOLDER || echo "Crave clone create failed!"
+else
+   mkdir $PROJECTFOLDER
+fi
+if grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"') && [ "${DCDEVSPACE}" == "1" ]; then
+   echo "Using Crave Clone... skipping"
+else
+   echo "Running $REPO_INIT"
+   $REPO_INIT
+fi
 
 # Run inside foss.crave.io devspace
 # Remove existing local_manifests
-crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
 
 # Clone local_manifests repository
 git clone https://github.com/sounddrill31/local_manifests --depth 1 -b lineage-oxygen .repo/local_manifests && \
 
  # Sync the repositories
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+ /opt/crave/resync.sh && \ 
 
 # Set up build environment
 source build/envsetup.sh && \
@@ -26,15 +45,10 @@ lunch lineage_oxygen-userdebug && \
 mka bacon"
 
 # Clean up
-rm -rf oxygen 
-
-
-
-# Pull generated zip files
-crave pull out/target/product/*/*.zip
-
-# Pull generated img files
-crave pull out/target/product/*/*.img 
-
+if grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"') && [ "${DCDEVSPACE}" == "1" ]; then
+  crave clone destroy -y $PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
+else  
+  rm -rf $PROJECTFOLDER || true
+fi
 # Upload zips to Telegram
-telegram-upload --to sdreleases oxygen/*.zip
+/opt/crave/telegram/upload.sh
